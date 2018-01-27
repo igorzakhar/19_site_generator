34942fb106e160fdfa806adcb40bc48c9ab204fc
igorzakhar <igorzakhar@gmail.com>
2018-01-27 18:13:37 +0300

    Adds get_templates() func

--- site_generator.py
+++ site_generator.py
@@ -1,7 +1,7 @@
 import json
 import os
 
-from jinja2 import Template
+from jinja2 import Environment, FileSystemLoader
 import markdown
 
 
@@ -10,14 +10,27 @@ def load_file(filename):
         return file.read()
 
 
-def render_index_page(json_data, html):
+def save_file(data):
+    with open('index_render.html', 'w') as file:
+        file.write(data)
+
+
+def render_index_page(json_data, template):
     articles = json_data['articles']
     topics = json_data['topics']
-    template = Template(html)
     return template.render(articles=articles, topics=topics)
+
+
+def get_templates():
+    env = Environment(loader=FileSystemLoader('templates'))
+    index_template = env.get_template('index.html')
+    artice_template = env.get_template('article.html')
+    return index_template, artice_template
 
 
 if __name__ == '__main__':
     json_data = json.loads(load_file('config.json'))
     html = load_file('templates/index.html')
-    render = render_index_page(json_data, html)
+    index_html, article_html = get_templates()
+    render = render_index_page(json_data, index_html)
+    save_file(render)
