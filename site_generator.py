aeced642a557123bf040561aa3cda67dc832ca89
igorzakhar <igorzakhar@gmail.com>
2018-01-28 15:21:58 +0300

    Adds generating html

--- site_generator.py
+++ site_generator.py
@@ -5,20 +5,21 @@ from jinja2 import Environment, FileSystemLoader
 import markdown
 
 
-def load_json_data(filename):
+BASE_DIR = 'articles/'
+SITE_DIR = 'site/'
+
+
+def load_file(filename):
     with open(filename, 'r') as file:
-        return json.load(file)
+        return file.read()
 
 
-def save_file(data):
-    with open('index_render.html', 'w') as file:
+def save_file(filename, data):
+    path_for_save = os.path.dirname(filename)
+    if not os.path.exists(path_for_save):
+        os.makedirs(path_for_save)
+    with open(filename, 'w') as file:
         file.write(data)
-
-
-def render_index_page(json_data, template):
-    articles = json_data['articles']
-    topics = json_data['topics']
-    return template.render(articles=articles, topics=topics)
 
 
 def get_templates():
@@ -28,8 +29,32 @@ def get_templates():
     return index_template, artice_template
 
 
+def render_index_page(json_data, template):
+    articles = json_data['articles']
+    topics = json_data['topics']
+    return template.render(articles=articles, topics=topics)
+
+
+def render_article_page(content, template):
+    return template.render(content)
+
+
+def main():
+    json_data = json.loads(load_file('config.json'))
+    index_template, article_template = get_templates()
+    index_html = render_index_page(json_data, index_template)
+    save_file('./index.html', index_html)
+
+    for article in json_data['articles']:
+        article_file = BASE_DIR + article['source']
+        html = markdown.markdown(load_file(article_file))
+        article_html = render_article_page(
+            {'title': article['title'], 'html': html},
+            article_template
+        )
+        save_path = SITE_DIR + article['source'].replace('md', 'html')
+        save_file(save_path, article_html)
+
+
 if __name__ == '__main__':
-    json_data = load_json_data('config.json')
-    index_html, article_html = get_templates()
-    render = render_index_page(json_data, index_html)
-    save_file(render)
+    main()
